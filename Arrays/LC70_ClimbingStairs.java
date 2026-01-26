0fb5b2dea9531656890601613194d396bb4c18d5
Md Masthan <mdmastan633@gmail.com>
2026-01-26 11:48:51 +0530

    Added LC70 SOlution

--- Arrays/LC70_ClimbingStairs.java
+++ Arrays/LC70_ClimbingStairs.java
@@ -7,16 +7,17 @@ Space complexity: O(1)
 
 class LC70_ClimbingStairs {
     public int climbStairs(int n) {
-        if (n <= 2) return n;
+        if (n == 1)
+            return 1;
+        if (n == 2)
+            return 2;
 
-        int prev1 = 2, prev2 = 1;
-
+        int a = 1, b = 2;
         for (int i = 3; i <= n; i++) {
-            int curr = prev1 + prev2;
-            prev2 = prev1;
-            prev1 = curr;
+            int c = a + b;
+            a = b;
+            b = c;
         }
-
-        return prev1;
+        return b;
     }
 }
