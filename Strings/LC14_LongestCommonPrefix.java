3418cbee6264a171a7a76fbbc5224528173dc99b
Md Masthan <mdmastan633@gmail.com>
2026-01-30 21:32:01 +0530

    Added Longest Common Prefix

---
+++ Strings/LC14_LongestCommonPrefix.java
@@ -0,0 +1,23 @@
+/*
+LeetCode 14 - Longest Common Prefix
+Approach: Vertical scanning (character by character comparison)
+Time complexity: O(N * M)
+Space complexity: O(1)
+*/
+
+public class LC14_LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+
+        for (int i = 0; i < strs[0].length(); i++) {
+            for (int j = 1; j < strs.length; j++) {
+                if (i >= strs[j].length() || strs[j].charAt(i) != strs[0].charAt(i)) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
+}
