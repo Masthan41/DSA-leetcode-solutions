a1275e7bc52b853b5fde8363f64cb45077743fc9
Md Masthan <mdmastan633@gmail.com>
2026-01-30 21:36:40 +0530

    Remove element from array

---
+++ Arrays/LC27_RemoveElement.java
@@ -0,0 +1,21 @@
+/*
+LeetCode 27 - Remove Element
+Approach: Two pointers (overwrite valid elements)
+Time complexity: O(n)
+Space complexity: O(1)
+*/
+
+class LC27_RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+}
