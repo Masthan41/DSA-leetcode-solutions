413994ec8d0201354b81d4730a324f0b89717e9c
Md Masthan <mdmastan633@gmail.com>
2026-02-01 23:33:02 +0530

    Added solutions for LeetCode 9 (Palindrome Number) and 69 (Sqrt X) using optimal approaches

---
+++ Math/LC9_PalindromeNumber.java
@@ -0,0 +1,30 @@
+/*
+LeetCode 9 - Palindrome Number
+Approach: Reverse half of the number
+Time Complexity: O(log10 n)
+Space Complexity: O(1)
+*/
+
+class LC9_PalindromeNumber {
+
+    public boolean isPalindrome(int x) {
+
+        // Negative numbers and numbers ending with 0 (except 0) are not palindrome
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int rev = 0;
+
+        // Reverse only half of the number
+        while (x > rev) {
+            int lastDigit = x % 10;
+            rev = rev * 10 + lastDigit;
+            x = x / 10;
+        }
+
+        // For even digits: x == rev
+        // For odd digits: x == rev / 10 (middle digit ignored)
+        return (x == rev) || (x == rev / 10);
+    }
+}
