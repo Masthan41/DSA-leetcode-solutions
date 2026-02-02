a1bdd76fbd6baf083a5e915323e6132c25897f11
Md Masthan <mdmastan633@gmail.com>
2026-02-02 22:19:04 +0530

    Added LC191 solutions

---
+++ BitManipulation/LC191_NumberOf1Bits.java
@@ -0,0 +1,17 @@
+/*
+LeetCode 191 - Number of 1 Bits
+Approach: Bit manipulation using n & (n-1)
+Time complexity: O(number of set bits)
+Space complexity: O(1)
+*/
+
+class LC191_NumberOf1Bits {
+    public int hammingWeight(int n) {
+        int c = 0;
+        while (n != 0) {
+            n = n & (n - 1);
+            c++;
+        }
+        return c;
+    }
+}
