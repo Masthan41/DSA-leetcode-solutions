641c1c87947d117f11b188e3c00ba38b481a5619
Md Masthan <mdmastan633@gmail.com>
2026-02-04 23:37:07 +0530

    Happy Number solution

--- Math/LC202_HappyNumber.java
+++ Math/LC202_HappyNumber.java
@@ -10,7 +10,7 @@ class LC202_HappyNumber {
         int sum = 0;
         while (n > 0) {
             int digit = n % 10;
-            sum += digit * digit;
+            sum = sum + digit * digit;
             n /= 10;
         }
         return sum;
