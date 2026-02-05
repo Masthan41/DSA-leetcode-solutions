ebe1b23e23537f79b0b00ed82918bf20053f7825
Md Masthan <mdmastan633@gmail.com>
2026-02-05 22:36:21 +0530

    Added Merge two Linked List solution

---
+++ Linked List/LC21_MergeTwoSortedLists.java
@@ -0,0 +1,45 @@
+/*
+Leetcode - 21 : Merge Two sorted linked lists
+Approach:
+Use a dummy node to build the merged list.
+Compare nodes from both lists and attach the smaller one.
+Move pointers accordingly.
+Finally, attach the remaining part of the non-empty list.
+
+Time Complexity: O(n + m)
+Space Complexity: O(1)
+*/
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class LC21_MergeTwoSortedLists {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                temp.next = list1;
+                list1 = list1.next;
+            } else {
+                temp.next = list2;
+                list2 = list2.next;
+            }
+            temp = temp.next;
+        }
+
+        temp.next = (list1 != null) ? list1 : list2;
+
+        return dummy.next;
+    }
+}
