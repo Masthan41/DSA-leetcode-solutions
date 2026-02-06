de658b144b5943cbc7a0674a095b09a248dc64b1
Md Masthan <mdmastan633@gmail.com>
2026-02-06 23:10:01 +0530

    Added Daily Tempertatures solution

---
+++ Stacks/LC739_DailyTemperatures.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class LC739_DailyTemperatures {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] ans = new int[temperatures.length];
+        Stack<Integer> s = new Stack<>();
+
+        for (int i = temperatures.length - 1; i >= 0; i--) {
+
+            while (!s.isEmpty() && temperatures[s.peek()] <= temperatures[i]) {
+                s.pop();
+            }
+
+            if (s.isEmpty()) {
+                ans[i] = 0;
+            } else {
+                ans[i] = s.peek() - i;
+            }
+
+            s.push(i);
+        }
+        return ans;
+    }
+}
