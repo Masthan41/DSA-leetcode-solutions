4e17f3657efe9fa1c0397b136e90f8bdb8439e80
Md Masthan <mdmastan633@gmail.com>
2026-02-07 21:39:30 +0530

    Added First Occurence solution

---
+++ Strings/LC28_FindFirstOccurrence.java
@@ -0,0 +1,29 @@
+/*
+LeetCode 28 - Find the Index of the First Occurrence in a String
+Approach: Sliding window substring comparison
+Time complexity: O(n * m)
+Space complexity: O(1)
+*/
+
+class LC28_FindFirstOccurrence {
+    public int strStr(String haystack, String needle) {
+        int n = haystack.length();
+        int m = needle.length();
+
+        if (m == 0)
+            return 0;
+
+        for (int i = 0; i <= n - m; i++) {
+            int j = 0;
+
+            while (j < m && haystack.charAt(i + j) == needle.charAt(j)) {
+                j++;
+            }
+
+            if (j == m)
+                return i;
+        }
+
+        return -1;
+    }
+}
