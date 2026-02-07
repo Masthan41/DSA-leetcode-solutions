66da036ac95050e03ad39fe5598ca0b8818e9e4b
Md Masthan <mdmastan633@gmail.com>
2026-02-07 21:36:51 +0530

    Added search Insert Position solution

--- Arrays/LC35_SearchInsertPosition.java
+++ Arrays/LC35_SearchInsertPosition.java
@@ -1,3 +1,26 @@
-public class LC35_SearchInsertPosition {
-    
+/*
+LeetCode 35 - Search Insert Position
+Approach: Binary Search
+Time complexity: O(log n)
+Space complexity: O(1)
+*/
+
+class LC35_SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return low;
+    }
 }
