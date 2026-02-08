18e88e5b854a511444ce5bd30ba4f589f25cf53c
Md Masthan <mdmastan633@gmail.com>
2026-02-08 21:05:48 +0530

    Added Valid Parentheses solution

---
+++ Stacks/LC20_ValidParentheses.java
@@ -0,0 +1,36 @@
+/*
+LeetCode 20 - Valid Parentheses
+Approach: Stack to match opening and closing brackets
+Time complexity: O(n)
+Space complexity: O(n)
+*/
+
+import java.util.*;
+
+class LC20_ValidParentheses {
+    Stack<Character> st = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            } else {
+                if (st.isEmpty()) {
+                    return false;
+                }
+                if ((st.peek() == '(' && ch == ')')
+                        || (st.peek() == '{' && ch == '}')
+                        || (st.peek() == '[' && ch == ']')) {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        if (st.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
