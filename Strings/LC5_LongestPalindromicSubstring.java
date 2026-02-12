271215165dc71ddfe263dd11713a098485cc6006
Md Masthan <mdmastan633@gmail.com>
2026-02-12 22:42:23 +0530

    Added Longest Palindromic SubString solution

--- Strings/LC5_LongestPalindromicSubstring.java
+++ Strings/LC5_LongestPalindromicSubstring.java
@@ -7,6 +7,14 @@ Space Complexity: O(1)
 */
 
 class LC5_LongestPalindromicSubstring {
+    private int expand(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && 
+               s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
 
     public String longestPalindrome(String s) {
         if (s == null || s.length() < 1) return "";
@@ -14,28 +22,15 @@ class LC5_LongestPalindromicSubstring {
         int start = 0, end = 0;
 
         for (int i = 0; i < s.length(); i++) {
-
-            int len1 = expandFromCenter(s, i, i);       // odd
-            int len2 = expandFromCenter(s, i, i + 1);   // even
+            int len1 = expand(s, i, i);
+            int len2 = expand(s, i, i + 1);
             int len = Math.max(len1, len2);
 
             if (len > end - start) {
                 start = i - (len - 1) / 2;
-                end = i + len / 2;
+                end   = i + len / 2;
             }
         }
-
         return s.substring(start, end + 1);
-    }
-
-    private int expandFromCenter(String s, int left, int right) {
-
-        while (left >= 0 && right < s.length()
-                && s.charAt(left) == s.charAt(right)) {
-            left--;
-            right++;
-        }
-
-        return right - left - 1;
     }
 }
