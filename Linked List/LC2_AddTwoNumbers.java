4e1795901f1f091431b786f36acd5c5344d76e3e
Md Masthan <mdmastan633@gmail.com>
2026-02-12 22:36:44 +0530

    Added Add two numbers solution

---
+++ Linked List/LC2_AddTwoNumbers.java
@@ -0,0 +1,46 @@
+/*
+LeetCode 2 - Add Two Numbers
+Approach:
+Simulate digit-by-digit addition like elementary math.
+Use carry for overflow.
+Time Complexity: O(max(m, n))
+Space Complexity: O(1)
+*/
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+class LC2_AddTwoNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+
+            int sum = carry;
+
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+        }
+
+        return dummy.next;
+    }
+}
