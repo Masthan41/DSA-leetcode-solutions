21ec034f64dc7561a17d67863c88ee5edc13c6fa
Md Masthan <mdmastan633@gmail.com>
2026-02-13 18:19:52 +0530

    Add LC11 Container With Most Water solution (Two Pointer)

---
+++ Arrays/LC11_ContainerWithMostWater.java
@@ -0,0 +1,20 @@
+public class LC11_ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        int l = 0, r = height.length - 1;
+        int maxArea = 0;
+        while (l < r) {
+            int h = Math.min(height[l], height[r]);
+            int width = r - l;
+            int area = h * width;
+
+            maxArea = Math.max(maxArea, area);
+
+            if (height[l] < height[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return maxArea;
+    }
+}
