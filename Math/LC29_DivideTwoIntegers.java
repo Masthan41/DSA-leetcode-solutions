cbae6939d0b9928f8d2df7b19d86cb2a9d0d6ed5
Md Masthan <mdmastan633@gmail.com>
2026-02-14 20:19:12 +0530

    Added Divide two Integers solution

--- Math/LC29_DivideTwoIntegers.java
+++ Math/LC29_DivideTwoIntegers.java
@@ -8,38 +8,31 @@ Space Complexity: O(1)
 class LC29_DivideTwoIntegers {
 
     public int divide(int dividend, int divisor) {
-
-        // Handle overflow case
+        // overflow check
         if (dividend == Integer.MIN_VALUE && divisor == -1) {
             return Integer.MAX_VALUE;
         }
-
-        // Convert to long and take absolute values
         long dvd = Math.abs((long) dividend);
         long dvs = Math.abs((long) divisor);
-
-        int quotient = 0;
+        long quo = 0;
 
         while (dvd >= dvs) {
-
             long temp = dvs;
-            int multiple = 1;
+            long mul = 1;
 
-            // Find largest shift
+            // laregst shift
             while (dvd >= (temp << 1)) {
                 temp <<= 1;
-                multiple <<= 1;
+                mul <<= 1;
             }
-
-            dvd -= temp;
-            quotient += multiple;
+            dvd = dvd - temp;
+            quo = quo + mul;
         }
-
         // Apply sign
         if ((dividend < 0) ^ (divisor < 0)) {
-            quotient = -quotient;
+            quo = -quo;
         }
 
-        return quotient;
+        return (int) quo;
     }
 }
