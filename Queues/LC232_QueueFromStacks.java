6983ab6c12dbbcfc3d120f7a78c8cf4fa76b71ae
Md Masthan <mdmastan633@gmail.com>
2026-02-15 15:16:57 +0530

    Added Queue from stacks solution

---
+++ Queues/LC232_QueueFromStacks.java
@@ -0,0 +1,58 @@
+/*
+LeetCode 232 - Implement Queue using Stacks
+Approach: Two stacks (input and output)
+Time Complexity:
+push  -> O(1)
+pop   -> O(1) amortized
+peek  -> O(1) amortized
+empty -> O(1)
+
+Space Complexity: O(n)
+*/
+
+import java.util.*;
+
+class LC232_QueueFromStacks {
+
+    Stack<Integer> input;
+    Stack<Integer> output;
+
+    public LC232_QueueFromStacks() {
+        input = new Stack<>();
+        output = new Stack<>();
+    }
+
+    // push element to back of queue
+    public void push(int x) {
+        input.push(x);
+    }
+
+    // removes front element
+    public int pop() {
+
+        if (output.isEmpty()) {
+            while (!input.isEmpty()) {
+                output.push(input.pop());
+            }
+        }
+
+        return output.pop();
+    }
+
+    // get front element
+    public int peek() {
+
+        if (output.isEmpty()) {
+            while (!input.isEmpty()) {
+                output.push(input.pop());
+            }
+        }
+
+        return output.peek();
+    }
+
+    // check if empty
+    public boolean empty() {
+        return input.isEmpty() && output.isEmpty();
+    }
+}
