a141a981c892b469cbc67dc72ce74978ab5874f2
Md Masthan <mdmastan633@gmail.com>
2026-02-16 18:50:33 +0530

    Added Majority Element solution

---
+++ Arrays/LC169_MajorityElement.java
@@ -0,0 +1,23 @@
+class LC169_MajorityElement {
+    public int majorityElement(int[] nums) {
+        int ans = nums[0];
+        int c = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (c == 0) {
+                ans = nums[i];
+            }
+
+            if (nums[i] == ans) {
+                c++;
+            } else {
+                c--;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        LC169_MajorityElement obj = new LC169_MajorityElement();
+        int[] nums = {2, 2, 1, 1, 1, 2, 2};
+        System.out.println(obj.majorityElement(nums));
+    }
+}
