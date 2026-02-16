062087e5c412a76725fe2a3d41da5936334b2f20
Md Masthan <mdmastan633@gmail.com>
2026-02-16 17:42:25 +0530

    Added Assign Cookies solution

--- Greedy Algorithms/LC455_AssignCookies.java
+++ Greedy Algorithms/LC455_AssignCookies.java
@@ -7,6 +7,26 @@ Time Complexity: O(n log n + m log m)
 Space Complexity: O(1)
 */
 
+import java.util.Arrays;
+
 public class LC455_AssignCookies {
-    
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int i = 0, j = 0;
+        while (i < g.length && j < s.length) {
+            if(s[j]>=g[i]){
+                i++;
+            }
+            j++;
+        }
+        return i;
+    }
+    public static void main(String[] args) {
+        LC455_AssignCookies s = new LC455_AssignCookies();
+        int[] g = {1,2,3};
+        int[] s1 = {1,1};
+        System.out.println(s.findContentChildren(g, s1)); // 1
+    }
 }
