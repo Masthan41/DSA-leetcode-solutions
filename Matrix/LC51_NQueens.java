3d18091ebd9506d918d90bd4e9ab53ac3f862d11
Md Masthan <mdmastan633@gmail.com>
2026-02-17 22:27:56 +0530

    Added N queens solution

---
+++ Matrix/LC51_NQueens.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LC51_NQueens {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> result = new ArrayList<>();
+        char[][] board = new char[n][n];
+
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(board[i], '.');
+        }
+
+        backtrack(board, 0, result);
+        return result;
+    }
+
+    private void backtrack(char[][] board, int row, List<List<String>> result) {
+        if (row == board.length) {
+            result.add(construct(board));
+            return;
+        }
+
+        for (int col = 0; col < board.length; col++) {
+            if (isSafe(board, row, col)) {
+                board[row][col] = 'Q';
+                backtrack(board, row + 1, result);
+                board[row][col] = '.'; // backtrack
+            }
+        }
+    }
+
+    private boolean isSafe(char[][] board, int row, int col) {
+        // vertical upwards
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q')
+                return false;
+        }
+
+        // left diagonal
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q')
+                return false;
+        }
+
+        // right diagonal
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q')
+                return false;
+        }
+
+        return true;
+    }
+
+    private List<String> construct(char[][] board) {
+        List<String> res = new ArrayList<>();
+        for (int i = 0; i < board.length; i++) {
+            res.add(new String(board[i]));
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        LC51_NQueens solution = new LC51_NQueens();
+        int n = 4;
+        List<List<String>> queens = solution.solveNQueens(n);
+        System.out.println(queens);
+    }
+}
