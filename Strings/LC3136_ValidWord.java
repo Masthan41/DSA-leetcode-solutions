cab7645e358218659d91333b13eb01015479fcb6
Md Masthan <mdmastan633@gmail.com>
2026-02-17 22:15:58 +0530

    Added Valid word solution

--- Strings/LC3136_ValidWord.java
+++ Strings/LC3136_ValidWord.java
@@ -1,16 +1,28 @@
 public class LC3136_ValidWord {
     public boolean isValid(String word) {
-        return word.length() >= 3 && word.chars().allMatch(Character::isLetterOrDigit) &&
-                word.chars().anyMatch(c -> isVowel((char) c)) &&
-                word.chars().anyMatch(c -> isConsonant((char) c));
-    }
 
-    private boolean isVowel(char c) {
-        return "aeiouAEIOU".indexOf(c) != -1;
-    }
+        if (word.length() < 3)
+            return false;
 
-    private boolean isConsonant(char c) {
-        return Character.isLetter(c) && !isVowel(c);
+        boolean hasVowel = false;
+        boolean hasConsonant = false;
+
+        for (char c : word.toCharArray()) {
+
+            // letter or digit
+            if (!Character.isLetterOrDigit(c))
+                return false;
+
+            // check vowel
+            if ("aeiouAEIOU".indexOf(c) != -1)
+                hasVowel = true;
+
+            // check consonant
+            else if (Character.isLetter(c))
+                hasConsonant = true;
+        }
+
+        return hasVowel && hasConsonant;
     }
     public static void main(String[] args) {
         LC3136_ValidWord solution = new LC3136_ValidWord();
