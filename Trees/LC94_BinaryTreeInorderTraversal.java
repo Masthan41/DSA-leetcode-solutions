345e7b3cb12dd52933b2b2ca320c1fa61a17a929
Md Masthan <mdmastan633@gmail.com>
2026-02-18 22:25:42 +0530

    Added Inorder Traversal solution

---
+++ Trees/LC94_BinaryTreeInorderTraversal.java
@@ -0,0 +1,41 @@
+/*
+LeetCode 94 - Binary Tree Inorder Traversal
+Approach: Inorder traversal (Left → Root → Right)
+Time Complexity: O(n)
+Space Complexity: 
+*/
+
+import java.util.*;
+
+public class LC94_BinaryTreeInorderTraversal {
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+
+        List<Integer> result = new ArrayList<>();
+        inorder(root, result);
+
+        return result;
+    }
+
+    private void inorder(TreeNode root, List<Integer> result) {
+
+        if (root == null)
+            return;
+
+        inorder(root.left, result);
+
+        result.add(root.val);
+
+        inorder(root.right, result);
+    }
+    public static void main(String[] args){
+        LC94_BinaryTreeInorderTraversal solution = new LC94_BinaryTreeInorderTraversal();
+
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+
+        List<Integer> result = solution.inorderTraversal(root);
+        System.out.println(result); // Output: [1, 3, 2]
+    }
+}
