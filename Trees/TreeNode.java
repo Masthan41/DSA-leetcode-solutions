16a4903af2000900c1db2afd89b4d4954e6272f8
Md Masthan <mdmastan633@gmail.com>
2026-02-18 22:27:04 +0530

    Added Inorder Traversal solution

---
+++ Trees/TreeNode.java
@@ -0,0 +1,22 @@
+/*
+TreeNode structure used in all Tree problems
+*/
+
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {}
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
