2ca4de38db06e9262558c2bed750f04d684c0972
Md Masthan <mdmastan633@gmail.com>
2026-02-22 21:46:08 +0530

    Added Max sum in BST code

---
+++ Trees/Binary Serach Trees/LC_1373_Maximum_Sum_BST.java
@@ -0,0 +1,54 @@
+
+class LC_1373_Maximum_Sum_BST {
+    class Info {
+        int min, max, sum;
+        boolean isBST;
+
+        Info(int min, int max, int sum, boolean isBST) {
+            this.min = min;
+            this.max = max;
+            this.sum = sum;
+            this.isBST = isBST;
+        }
+    }
+
+    int maxSum = 0;
+
+    public int maxSumBST(TreeNode root) {
+        helper(root);
+        return maxSum;
+    }
+
+    private Info helper(TreeNode root) {
+        if (root == null)
+            return new Info(Integer.MAX_VALUE, Integer.MIN_VALUE, 0, true);
+
+        Info left = helper(root.left);
+        Info right = helper(root.right);
+
+        if (left.isBST && right.isBST && root.val > left.max && root.val < right.min) {
+            int curr = left.sum + right.sum + root.val;
+            maxSum = Math.max(maxSum, curr);
+
+            int min = Math.min(root.val, left.min);
+            int max = Math.max(root.val, right.max);
+
+            return new Info(min, max, curr, true);
+        }
+        return new Info(0, 0, 0, false);
+    }
+    public static void main(String[] args) {
+        LC_1373_Maximum_Sum_BST solution = new LC_1373_Maximum_Sum_BST();
+
+        // Example usage:
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(2);
+        root.left.right = new TreeNode(4);
+        root.right.right = new TreeNode(5);
+
+        int result = solution.maxSumBST(root);
+        System.out.println("Maximum Sum BST in the Binary Tree: " + result);
+    }
+}
--- Trees/Binary Serach Trees/TreeNode.java
+++ Trees/Binary Serach Trees/TreeNode.java
@@ -1,17 +1,16 @@
-
+/*Used in all tree problems */
 
 public class TreeNode {
-
     int val;
     TreeNode left;
     TreeNode right;
-
+    
     TreeNode() {}
-
+    
     TreeNode(int val) {
         this.val = val;
     }
-
+    
     TreeNode(int val, TreeNode left, TreeNode right) {
         this.val = val;
         this.left = left;
