9e708ef97d06663664df1449bba3477cf7c9fc56
Md Masthan <mdmastan633@gmail.com>
2026-02-23 21:08:19 +0530

    Added Daigonal sum of matrix code

---
+++ Matrix/LC1572_DiagonalSum.java
@@ -0,0 +1,22 @@
+class LC1572_DiagonalSum {
+    public int diagonalSum(int[][] mat) {
+        int sum = 0;
+        for (int i = 0; i < mat.length; i++) {
+            sum += mat[i][i];
+
+            if (i != mat.length - 1 - i)
+                sum += mat[i][mat.length - i - 1];
+
+            // throw exception
+            if (mat.length != mat[0].length)
+                throw new IllegalArgumentException("Matrix must be square");
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        LC1572_DiagonalSum obj = new LC1572_DiagonalSum();
+        int[][] mat = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        System.out.println(obj.diagonalSum(mat)); // Output: 25
+    }
+}
