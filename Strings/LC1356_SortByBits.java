34a058180558740e0d9b58020c9885c71ab416d1
Md Masthan <mdmastan633@gmail.com>
2026-02-25 22:17:12 +0530

    Added Sort by bits code

---
+++ Strings/LC1356_SortByBits.java
@@ -0,0 +1,32 @@
+/*
+LeetCode 1356 - Sort Integers by The Number of 1 Bits
+Approach: Sort with custom comparator
+Time complexity: O(n log n)
+Space complexity: O(n)
+*/
+
+import java.util.*;
+
+class LC1356_SortByBits {
+    public int[] sortByBits(int[] arr) {
+        Integer nums[] = new Integer[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            nums[i] = arr[i];
+        }
+        Arrays.sort(nums, new Comparator<Integer>() {
+            public int compare(Integer a, Integer b) {
+                int bita = Integer.bitCount(a);
+                int bitb = Integer.bitCount(b);
+
+                if (bita == bitb) {
+                    return a - b;
+                }
+                return bita - bitb;
+            }
+        });
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = nums[i];
+        }
+        return arr;
+    }
+}
