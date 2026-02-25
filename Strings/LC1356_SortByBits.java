800e6a29ff28f94c28c21f6f497525c98f8c9016
Md Masthan <mdmastan633@gmail.com>
2026-02-25 22:17:26 +0530

    Added Sort by bits code

--- Strings/LC1356_SortByBits.java
+++ Strings/LC1356_SortByBits.java
@@ -29,4 +29,9 @@ class LC1356_SortByBits {
         }
         return arr;
     }
+    public static void main(String[] args) {
+        LC1356_SortByBits s = new LC1356_SortByBits();
+        int[] arr = {0,1,2,3,4,5,6,7,8};
+        System.out.println(Arrays.toString(s.sortByBits(arr)));
+    }
 }
