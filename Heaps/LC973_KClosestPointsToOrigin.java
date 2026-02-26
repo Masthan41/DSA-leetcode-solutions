fadb6f73de7551e250d1e69ae0d234fd34b9933a
Md Masthan <mdmastan633@gmail.com>
2026-02-26 22:40:34 +0530

    Added K closest point to origin code

--- Heaps/LC973_KClosestPointsToOrigin.java
+++ Heaps/LC973_KClosestPointsToOrigin.java
@@ -1,10 +1,37 @@
 /*
-LeetCode 693 - Binary Number with Alternating Bits
-Approach: Bit Manipulation
-Time Complexity: O(log n)
-Space Complexity: O(1)
+LeetCode 973 - K Closest Points to Origin
+Approach: Use a max-heap to keep track of the k closest points.
+Time Complexity: O(n log k)
+Space Complexity: O(k)
 */
 
+import java.util.*;
+
 class LC973_KClosestPointsToOrigin {
-    
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+                (a, b) -> (b[0] * b[0] + b[1] * b[1]) - (a[0] * a[0] + a[1] * a[1]));
+        for (int pt[] : points) {
+            pq.add(pt);
+
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+        int res[][] = new int[k][2];
+        for (int i = 0; i < k; i++) {
+            res[i] = pq.poll();
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        LC973_KClosestPointsToOrigin obj = new LC973_KClosestPointsToOrigin();
+        int points[][] = { { 1, 3 }, { -2, 2 } };
+        int k = 1;
+        int res[][] = obj.kClosest(points, k);
+        for (int i = 0; i < res.length; i++) {
+            System.out.println(res[i][0] + " " + res[i][1]);
+        }
+    }
 }
