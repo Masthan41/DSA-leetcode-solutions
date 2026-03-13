4ad2a6905084349b9a7083ea6f229c77238fbf1f
Md Masthan <mdmastan633@gmail.com>
2026-03-13 23:32:28 +0530

    Added Min Seconds to make mountain height 0 code

--- BinarySearch/LC3296_MinSeconds.java
+++ BinarySearch/LC3296_MinSeconds.java
@@ -1,3 +1,13 @@
+/*
+LeetCode 3296 - Minimum Seconds to make Mountain height 0
+Pattern: Binary Search + check function to calculate total height reduced by workers in given seconds
+Approach: Identify sorted half and discard the other half
+
+Time Complexity: O(n log(maxTime * mountainHeight^2)) where n is the number of workers and maxTime is the maximum time taken by any worker.
+
+Space Complexity: O(1) as we are using only constant extra space.
+*/
+
 class LC3296_MinSeconds {
     public boolean check(long mid, int wt[], int mh) {
         long h = 0;
