ea266b9c8bb8eb482da1578996c3aee17d3b65bc
Md Masthan <mdmastan633@gmail.com>
2026-03-20 23:51:12 +0530

    Added Longest sunstring without repeating characters code

--- Strings/LC3_LongestSubstring.java
+++ Strings/LC3_LongestSubstring.java
@@ -7,7 +7,7 @@ Space Complexity: O(min(n, charset))
 
 import java.util.HashSet;
 
-class LC3_LongestSubstringWithoutRepeatingCharacters {
+class LC3_LongestSubstring {
 
     public int lengthOfLongestSubstring(String s) {
         
@@ -22,5 +22,11 @@ class LC3_LongestSubstringWithoutRepeatingCharacters {
             maxL = Math.max(maxL, r - l + 1);
         }
         return maxL;
+    }
+    public static void main(String[] args) {
+        LC3_LongestSubstring solution = new LC3_LongestSubstring();
+        String s = "abcabcbb";
+        int result = solution.lengthOfLongestSubstring(s);
+        System.out.println(result);
     }
 }
