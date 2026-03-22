dfe74ba1eca25d4607fe743f28217addceac16a7
Md Masthan <mdmastan633@gmail.com>
2026-03-22 23:17:33 +0530

    Added Two sum code

--- Arrays/LC1_TwoSum.java
+++ Arrays/LC1_TwoSum.java
@@ -1,23 +1,28 @@
 /*
 LeetCode 1 - Two Sum
-Approach:
-Use HashMap to store visited numbers and their indices.
+Approach:Use HashMap to store visited numbers and their indices.
 For each element, check if (target - current) exists.
+
 Time Complexity: O(n)
 Space Complexity: O(n)
 */
 
+import java.util.*;
+
 class LC1_TwoSum {
     public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[] { i, j };
-                }
+            int c = target - nums[i];
+
+            if (map.containsKey(c)) {
+                return new int[] { map.get(c), i };
             }
+            map.put(nums[i], i);
         }
         return new int[] {};
     }
+
     public static void main(String[] args) {
         LC1_TwoSum obj = new LC1_TwoSum();
         int[] nums = { 2, 7, 11, 15 };
