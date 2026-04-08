7ceca97e6929256c8a50369fac233df7a5affb93
Md Masthan <mdmastan633@gmail.com>
2026-04-08 16:01:10 +0530

    Added Robot return to origin code

--- Strings/LC657_ReturnToOrigin.java
+++ Strings/LC657_ReturnToOrigin.java
@@ -1,3 +1,24 @@
 class LC657_ReturnToOrigin {
-    
+    public boolean judgeCircle(String moves) {
+        int x = 0, y = 0;
+        for (int i = 0; i < moves.length(); i++) {
+            if (moves.charAt(i) == 'U') {
+                y++;
+            } else if (moves.charAt(i) == 'D') {
+                y--;
+            } else if (moves.charAt(i) == 'L') {
+                x--;
+            } else {
+                x++;
+            }
+        }
+        return x == 0 && y == 0;
+    }
+
+    public static void main(String[] args) {
+        LC657_ReturnToOrigin solution = new LC657_ReturnToOrigin();
+        String moves = "UD";
+        boolean result = solution.judgeCircle(moves);
+        System.out.println(result); // Output: true
+    }
 }
