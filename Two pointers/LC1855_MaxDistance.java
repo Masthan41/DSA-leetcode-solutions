7b96946c63cfc2a037a67b8bd12c28b76cc700e9
Md Masthan <mdmastan633@gmail.com>
2026-04-19 19:49:58 +0530

    Added Max Distance code

---
+++ Two pointers/LC1855_MaxDistance.java
@@ -0,0 +1,28 @@
+class LC1855_MaxDistance{
+    public int maxDistance(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+
+        int i = 0; // nums1 pointer
+        int j = 0; // nums2 pointer
+
+        int result = 0;
+
+        while (i < m && j < n) {
+            if (nums1[i] > nums2[j]) {
+                i++;
+            } else {
+                result = Math.max(result, j - i);
+                j++;
+            }
+        }
+
+        return result;
+    }
+    public static void main(String[] args) {
+        LC1855_MaxDistance obj = new LC1855_MaxDistance();
+        int[] nums1 = {1, 2, 3};
+        int[] nums2 = {3, 4, 5};
+        System.out.println("Maximum Distance: " + obj.maxDistance(nums1, nums2));
+    }
+}
