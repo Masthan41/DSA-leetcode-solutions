2f17dc05d9cb31e705196ac7b4fe55d71f03d12e
Md Masthan <mdmastan633@gmail.com>
2026-04-19 19:50:59 +0530

    Added Max Distance code

--- Two pointers/LC1855_MaxDistance.java
+++ Two pointers/LC1855_MaxDistance.java
@@ -1,3 +1,11 @@
+/*
+LeetCode 1855 - Maximum Distance Between Elements
+Approach: Use two pointers to traverse both arrays and find the maximum distance between elements where i <= j and nums1[i] <= nums2[j].
+
+Time Complexity: O(m + n) where m and n are the lengths of the two arrays.
+Space Complexity: O(1) as we only use a constant amount of extra space.
+*/
+
 class LC1855_MaxDistance{
     public int maxDistance(int[] nums1, int[] nums2) {
         int m = nums1.length;
