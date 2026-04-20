3a186da34b9dfab4f767169e50c64abb6581f0e0
Md Masthan <mdmastan633@gmail.com>
2026-04-20 20:26:30 +0530

    Added Two furthest houses code

--- Arrays/LC2078_FurthestHouses.java
+++ Arrays/LC2078_FurthestHouses.java
@@ -1,3 +1,11 @@
+/*
+LeetCode 2078 - Furthest Houses
+Approach: To find the furthest houses with different colors, we can iterate through the array and keep track of the maximum distance between houses of different colors.
+
+Time Complexity: O(n) where n is the length of the colors array.
+Space Complexity: O(1) as we only use a constant amount of extra space.
+*/
+
 class LC2078_FurthestHouses {
     public int maxDistance(int[] colors) {
         int n = colors.length;
