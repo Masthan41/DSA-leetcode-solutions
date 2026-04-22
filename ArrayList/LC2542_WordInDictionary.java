75a445f588cb45d610d0a6aba36cd51645eacaae
Md Masthan <mdmastan633@gmail.com>
2026-04-22 22:14:44 +0530

    Added Words within two dictonaries code

---
+++ ArrayList/LC2542_WordInDictionary.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class LC2542_WordInDictionary {
+    public List<String> twoEditWords(String[] queries, String[] dictionary) {
+        List<String> list = new ArrayList<>();
+
+        for (String query : queries) {
+            for (String s : dictionary) {
+                int diff = 0;
+                for (int i = 0; i < query.length(); i++) {
+                    if (query.charAt(i) != s.charAt(i)) {
+                        diff++;
+                    }
+                    if (diff > 2)
+                        break;
+                }
+                if (diff <= 2) {
+                    list.add(query);
+                    break;
+                }
+            }
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        LC2542_WordInDictionary solution = new LC2542_WordInDictionary();
+        String[] queries = { "word", "note", "ants", "wood" };
+        String[] dictionary = { "wood", "joke", "moat" };
+        List<String> result = solution.twoEditWords(queries, dictionary);
+        System.out.println(result);
+    }
+}
