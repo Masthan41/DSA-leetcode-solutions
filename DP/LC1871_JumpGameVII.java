29ac25b18ac838e9fcc065a7b5569c0dcfa8a3d0
Md Masthan <mdmastan633@gmail.com>
2026-05-25 22:52:04 +0530

    Added Jump game VII code

--- DP/LC1871_JumpGameVII.java
+++ DP/LC1871_JumpGameVII.java
@@ -1,3 +1,11 @@
+/*
+LeetCode 1871 - Jump Game VII
+Approach: We can use dynamic programming to solve this problem. We create an array `t` where `t[i]` represents whether it is possible to reach index `i`. We iterate through the string and for each index, we check if it can be reached from any of the previous indices within the range defined by `minJump` and `maxJump`. We maintain a count of reachable indices to efficiently determine if the current index can be reached. If the current index is reachable and the character at that index is '0', we mark it as reachable in our `t` array.
+
+Time Complexity: O(n) since we iterate through the string once and maintain a count of reachable indices.
+Space Complexity : O(n) for the `t` array.
+*/
+
 class LC1871_JumpGameVII{
     public boolean canReach(String s, int minJump, int maxJump) {
         int n = s.length();
