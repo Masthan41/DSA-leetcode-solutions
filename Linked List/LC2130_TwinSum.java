d9e88b00c96a3f27b98ffa761d29cd5f26b13e3d
Md Masthan <mdmastan633@gmail.com>
2026-06-16 17:34:32 +0530

    Added Maximum Twin Sum code

--- Linked List/LC2130_TwinSum.java
+++ Linked List/LC2130_TwinSum.java
@@ -1,9 +1,8 @@
 /*
 Leetcode - 2130 : Maximum Twin Sum of a Linked List
-Approach: Use a stack to store the values of the first half of the linked list. Then, iterate through the second half and calculate the sum of each twin with the corresponding value from the stack.
-
-Time Complexity: O(n) as we traverse the linked list twice, once to push values onto the stack and once to calculate the twin sums.
-Space Complexity: O(n/2) = O(n) for the stack.
+Approach: Use the fast and slow pointer technique to find the middle of the linked list. Then reverse the second half of the linked list. Finally, iterate through both halves of the linked list simultaneously to calculate the twin sums and keep track of the maximum twin sum.
+Time Complexity: O(n) as we traverse the linked list a few times (finding the middle, reversing the second half, and calculating twin sums).
+Space Complexity: O(1) for the pointers used in reversing the second half.
 */
 
 import java.util.*;
@@ -27,24 +26,35 @@ class ListNode {
 
 class LC2130_TwinSum {
     public int pairSum(ListNode head) {
-        Stack<Integer> st = new Stack<>();
+        ListNode mid = null;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        mid = slow;
+
+        //reverse the second half
+        ListNode nextNode = null;
+        ListNode prev = null;
+
+        while (mid != null) {
+            nextNode = mid.next;
+            mid.next = prev;
+            prev = mid;
+            mid = nextNode;
+        }
+
         ListNode curr = head;
-
-        while (curr != null) {
-            st.push(curr.val);
+        int result = 0;
+        while (prev != null) {
+            result = Math.max(result, (curr.val + prev.val));
             curr = curr.next;
+            prev = prev.next;
         }
-
-        int maxSum = Integer.MIN_VALUE;
-        curr = head;
-        int count = 0;
-        int n = st.size();
-        while (count < n / 2) {
-            maxSum = Math.max(maxSum, (curr.val + st.pop()));
-            curr = curr.next;
-            count++;
-        }
-        return maxSum;
+        return result;
     }
 
     public static void main(String[] args) {
