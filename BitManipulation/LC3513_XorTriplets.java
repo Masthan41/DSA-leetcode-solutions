e95c65aeefbcf496a636080ca84c98b7fb309b7b
Md Masthan <mdmastan633@gmail.com>
2026-07-23 23:43:24 +0530

    Added Unique XOR triplets I code

--- BitManipulation/LC3513_XorTriplets.java
+++ BitManipulation/LC3513_XorTriplets.java
@@ -1,3 +1,11 @@
+/*
+LeetCode 3513 - Unique XOR Triplets I
+Approach: The approach to solve this problem is based on the observation that the number of unique XOR triplets can be determined by the length of the input array. If the length of the array is 1 or 2, the number of unique triplets is equal to the length of the array. For lengths greater than 2, we can calculate the smallest power of 2 that is greater than or equal to the length of the array, which gives us the number of unique XOR triplets.
+
+Time Complexity: O(log n) since we are calculating the smallest power of 2 greater than or equal to n, which can be done in logarithmic time.
+Space Complexity: O(1) since we are using a constant amount of space to store the answer and the length of the array.
+*/
+
 class LC3513_XorTriplets {
     public int uniqueXorTriplets(int[] nums) {
         int n = nums.length;
@@ -16,6 +24,6 @@ class LC3513_XorTriplets {
         LC3513_XorTriplets s = new LC3513_XorTriplets();
         int[] nums = { 1, 2, 3 };
         System.out.println(s.uniqueXorTriplets(nums));
-        
-    }  
+
+    }
 }
