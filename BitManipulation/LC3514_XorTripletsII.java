af7986f94e8f94d6fb7e8f65c834e028e3efc200
Md Masthan <mdmastan633@gmail.com>
2026-07-24 19:53:09 +0530

    Added Unique XOR triplets II code

--- BitManipulation/LC3514_XorTripletsII.java
+++ BitManipulation/LC3514_XorTripletsII.java
@@ -1,5 +1,13 @@
+/*
+LeetCode 3514 - Unique XOR Triplets II
+Approach: The approach to solve this problem is based on the observation that we can use two boolean arrays to keep track of the unique XOR values. The first boolean array, s1, is used to store the XOR values of all pairs of elements in the input array. The second boolean array, s2, is used to store the XOR values of the elements in s1 with each element in the input array. Finally, we count the number of true values in s2 to get the number of unique XOR triplets.
+
+Time Complexity: O(n^2) since we are iterating through all pairs of elements in the input array to calculate the XOR values and then iterating through the boolean arrays to count the unique values.
+Space Complexity: O(T) where T is the smallest power of 2 greater than or equal to the maximum element in the input array, since we are using two boolean arrays of size T to store the XOR values.
+*/
+
 class LC3514_XorTripletsII {
-    ublic int uniqueXorTriplets(int[] nums) {
+    public int uniqueXorTriplets(int[] nums) {
         int n = nums.length;
         int maxEle = 0;
         for (int num : nums) {
@@ -36,6 +44,7 @@ class LC3514_XorTripletsII {
         }
         return count;
     }
+
     public static void main(String[] a) {
         LC3514_XorTripletsII s = new LC3514_XorTripletsII();
         int[] nums = { 1, 2, 3 };
