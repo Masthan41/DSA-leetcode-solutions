3046053549b9716155cccb526d2fa73f59fbed81
Md Masthan <mdmastan633@gmail.com>
2026-07-28 22:31:56 +0530

    Added Smallest Rearrangement I code

--- Sorting/LC3517_SmallestRearrangementI.java
+++ Sorting/LC3517_SmallestRearrangementI.java
@@ -1,4 +1,12 @@
-import java.util.Arrays;
+/*
+LeetCode 3517 - Smallest Palindromic Rearrangement I
+Approach: Sort the characters in ascending order. Iterate through the sorted array and check if the current character is less than the character at the corresponding position from the end. If it is, return false. If the character is greater than or equal to 1e5, return true (since we won't encounter any larger characters). Otherwise, add the character's mass to the current mass and continue iterating.
+
+Time complexity: O(n log n) due to sorting the characters array.
+Space complexity: O(n) since we are using a new array to store the sorted characters.
+*/
+
+import java.util.*;
 
 class LC3517_SmallestRearrangementI {
     public String smallestPalindrome(String s) {
