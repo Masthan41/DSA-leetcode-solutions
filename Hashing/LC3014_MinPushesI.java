6232515677cc8d230e77b479acc11c7177f378a5
Md Masthan <mdmastan633@gmail.com>
2026-07-30 22:20:16 +0530

    Added Min Pushes for given word I code

--- Hashing/LC3014_MinPushesI.java
+++ Hashing/LC3014_MinPushesI.java
@@ -1,4 +1,11 @@
-import java.util.HashMap;
+/*
+LeetCode 3014 - Minimum Number of Pushes to Type a String I
+Approach: HashMap Frequency Counting through assigning each character to a number from 2 to 9 and counting the total pushes required.
+Time Complexity: O(n) as we iterate through the string once and perform constant time operations for each character.
+Space Complexity: O(1) since the HashMap will have at most 8 entries (for digits 2 to 9).
+*/
+
+import java.util.*;
 
 class LC3014_MinPushesI {
     public int minimumPushes(String word) {
@@ -22,10 +29,11 @@ class LC3014_MinPushesI {
         }
         return count;
     }
+
     public static void main(String[] args) {
         LC3014_MinPushesI solution = new LC3014_MinPushesI();
         String word = "abcdefghijklmno";
         int result = solution.minimumPushes(word);
         System.out.println("Minimum pushes for the word '" + word + "': " + result);
-    }    
+    }
 }
