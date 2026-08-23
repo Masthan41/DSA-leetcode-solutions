f798e65c5caa96ad94b0a1e99f2bfc4a917b0bee
Md Masthan <mdmastan633@gmail.com>
2026-08-23 21:52:58 +0530

    Added Sum game code

--- Greedy Algorithms/LC1927_SumGame.java
+++ Greedy Algorithms/LC1927_SumGame.java
@@ -1,7 +1,15 @@
-class LC1327_SumGame {
+/*
+LeetCode 1927 - Sum Game
+Approach : Use a Greedy approach how many questions marks are there and what is the known sum on both sides. After that use math derivation to solve the problem further
+Time Complexity: O(n) as we iterate over the string only once
+Space Complexity: O(1) as we use only varaibles
+*/
+
+
+class LC1927_SumGame {
     public boolean sumGame(String num) {
         int n = num.length();
-        int leftKnownSum = 0, rightKnowmSum = 0;
+        int leftKnownSum = 0, rightKnownSum = 0;
         int leftQuestionMarks = 0, rightQuestionMarks = 0;
 
         for (int i = 0; i < n; i++) {
@@ -15,7 +23,7 @@ class LC1327_SumGame {
                 if (i < n / 2) {
                     leftKnownSum += num.charAt(i) - '0';
                 } else {
-                    rightKnowmSum += num.charAt(i) - '0';
+                    rightKnownSum += num.charAt(i) - '0';
                 }
             }
         }
@@ -25,7 +33,7 @@ class LC1327_SumGame {
         }
 
         int LEFT = 2 * leftKnownSum + 9 * leftQuestionMarks;
-        int RIGHT = 2 * rightKnowmSum + 9 * rightQuestionMarks;
+        int RIGHT = 2 * rightKnownSum + 9 * rightQuestionMarks;
 
         if (LEFT == RIGHT) {
             return false;
@@ -34,7 +42,7 @@ class LC1327_SumGame {
     }
 
     public static void main(String[] args) {
-        LC1327_SumGame solution = new LC1327_SumGame();
+        LC1927_SumGame solution = new LC1927_SumGame();
         String num = "25??";
         boolean result = solution.sumGame(num);
         System.out.println("Result: " + result); // Output: Result: true
