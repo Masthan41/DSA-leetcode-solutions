137efe42d60c761550c0e4cef033bb11adf0e343
Md Masthan <mdmastan633@gmail.com>
2026-08-25 22:03:16 +0530

    Added Missing multiple of k code

--- Math/LC3718_MissingMultiple.java
+++ Math/LC3718_MissingMultiple.java
@@ -1,3 +1,12 @@
+/*
+LeetCode 3718 - Missing Multiple of K
+Approach: We can use a HashSet to store the elements of the input array `nums`. Then, we start checking multiples of `k` starting from `k` itself. We keep incrementing by `k` until we find a multiple that is not present in the HashSet. This will be the smallest missing multiple of `k`.
+Time Complexity: O(n) where n is the length of the input array `nums`, as we need to iterate through the array to populate the HashSet and then check for multiples of `k`.
+Space Complexity: O(n) where n is the length of the input array `nums`, as we need to store all elements in the HashSet.
+*/
+
+import java.util.*;
+
 class LC3718_MissingMultiple {
     public int missingMultiple(int[] nums, int k) {
         int n = nums.length;
@@ -17,5 +26,6 @@ class LC3718_MissingMultiple {
         int[] nums = { 1, 2, 3, 4, 5 };
         int k = 2;
         int result = solution.missingMultiple(nums, k);
+        System.out.println("The smallest missing multiple of " + k + " is: " + result);
     }
 }
