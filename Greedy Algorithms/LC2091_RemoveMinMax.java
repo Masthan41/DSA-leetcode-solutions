62df70c3e5d4a630b35aa8f222806f704c5fc2e3
Md Masthan <mdmastan633@gmail.com>
2026-08-30 21:59:53 +0530

    Added Remove minimum and maximum element from array code

---
+++ Greedy Algorithms/LC2091_RemoveMinMax.java
@@ -0,0 +1,27 @@
+class LC2091_RemoveMinMax {
+    public int minimumDeletions(int[] nums) {
+        int n = nums.length;
+        int minIdx = 0, maxIdx = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] < nums[minIdx]) {
+                minIdx = i;
+            }
+            if (nums[i] > nums[maxIdx]) {
+                maxIdx = i;
+            }
+        }
+
+        int left = Math.min(minIdx, maxIdx);
+        int right = Math.max(minIdx, maxIdx);
+
+        return Math.min(left + 1 + n - right, Math.min(right + 1, n - left));
+    }
+
+    public static void main(String[] args) {
+        LC2091_RemoveMinMax solution = new LC2091_RemoveMinMax();
+        int[] nums = { 2, 10, 7, 5, 4, 1, 8, 6 };
+        int result = solution.minimumDeletions(nums);
+        System.out.println("Minimum deletions: " + result);
+    }
+}
