d9f916789460ca1632df15f4e46826c6d5aecadb
Md Masthan <mdmastan633@gmail.com>
2026-09-04 19:54:18 +0530

    Added Smallest stable index I code

--- Prefix Sum/LC3903_StableIndexI.java
+++ Prefix Sum/LC3903_StableIndexI.java
@@ -1,3 +1,12 @@
+/*
+LeetCode 3903 - Smallest stable index I
+Approach: Use a brute-force approach to find the first stable index. For each index, we check the maximum value to the left and the minimum value to the right. If the difference between them is less than or equal to k, we return that index.
+Time Complexity: O(n^2) where n is the length of the array nums. We check each index and for each index, we may traverse the entire array to find the left maximum and right minimum.
+Space Complexity: O(1) as we are using a constant amount of extra space.
+
+optimized approach is there in second variation of this problem
+*/
+
 class LC3903_StableIndexI {
     public int firstStableIndex(int[] nums, int k) {
         int n = nums.length;
