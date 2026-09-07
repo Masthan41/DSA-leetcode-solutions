07f7003c5ca257c48a64cd7f15503797bb060ea5
Md Masthan <mdmastan633@gmail.com>
2026-09-07 22:43:56 +0530

    Added Distinct Subsequences I code

--- DP/LC115_DistinctSubsequencesI.java
+++ DP/LC115_DistinctSubsequencesI.java
@@ -1,11 +1,13 @@
+/*
 LeetCode 115 - Distinct Subsequences
 Approach: We can use dynamic programming to solve this problem. We will create a 2D array dp where dp[i][j] represents the number of distinct subsequences of the first i characters of string s that equals the first j characters of string t.
+
 Time Complexity: O(m * n) where m is the length of string s and n is the length of string t, as we need to fill the dp array of size (m+1) x (n+1).
 Space Complexity: O(m * n) for the dp array.
 */
 
 
-class LC115_DistinctSubsequences {
+class LC115_DistinctSubsequencesI {
     long dp[][] = new long[1001][1001];
 
     public int numDistinct(String s, String t) {
@@ -32,7 +34,7 @@ class LC115_DistinctSubsequences {
     }
 
     public static void main(String[] args) {
-        LC115_DistinctSubsequences solution = new LC115_DistinctSubsequences();
+        LC115_DistinctSubsequencesI solution = new LC115_DistinctSubsequencesI();
         String s = "rabbbit";
         String t = "rabbit";
         int result = solution.numDistinct(s, t);
