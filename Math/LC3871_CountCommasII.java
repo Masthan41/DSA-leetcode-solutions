7cdf5df6ea174b476f95430e72afa391e2b838da
Md Masthan <mdmastan633@gmail.com>
2026-09-09 21:32:45 +0530

    Added Count commas in given range II code

--- Math/LC3871_CountCommasII.java
+++ Math/LC3871_CountCommasII.java
@@ -1,4 +1,11 @@
-class LC3871_CountCommasII{
+/*
+LeetCode 3871 - Count commas in range II
+Approach: Use a loop to count the number of commas in the range from 1 to n. We start counting from 1000 and keep multiplying by 1000 until we exceed n. For each range, we add the count of numbers that have commas to the result.
+Time complexity: O(log n) because we are dividing the range by 1000 in each iteration of the loop.
+Space complexity: O(1) because we are using a constant amount of space.
+*/
+
+class LC3871_CountCommasII {
     public long countCommas(long n) {
         long result = 0;
         long start = 1000;
@@ -9,7 +16,8 @@ class LC3871_CountCommasII{
         }
         return result;
     }
-    public static void main(String[] a){
+
+    public static void main(String[] a) {
         LC3871_CountCommasII solution = new LC3871_CountCommasII();
         System.out.println(solution.countCommas(1000)); // Output: 1
         System.out.println(solution.countCommas(999)); // Output: 0
