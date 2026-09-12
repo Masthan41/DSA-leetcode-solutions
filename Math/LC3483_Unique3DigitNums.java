d61acb7725e59dd3dce03b2cf8bebed4c6d4e383
Md Masthan <mdmastan633@gmail.com>
2026-09-12 15:37:59 +0530

    Added Unique 3 digit even numbers code

--- Math/LC3483_Unique3DigitNums.java
+++ Math/LC3483_Unique3DigitNums.java
@@ -1,3 +1,10 @@
+/*
+LeetCode 3483 - Unique 3-Digit Even Numbers
+Approach: Find all unique 3-digit numbers that can be formed using the given digits.
+Time Complexity: O(n^3) where n is the number of digits, since we need to check combinations of three digits.
+Space Complexity: O(1) since we are using a constant amount of space for counting digits.   
+*/
+
 class LC3483_Unique3DigitNums {
     public int totalNumbers(int[] digits) {
         int[] count = new int[10];
@@ -17,36 +24,38 @@ class LC3483_Unique3DigitNums {
 
             // Check if we have enough copies of each digit
             if (count[ones] > 0 &&
-                count[tens] > 0 &&
-                count[hundreds] > 0) {
+                    count[tens] > 0 &&
+                    count[hundreds] > 0) {
 
                 // If digits are same, we need multiple copies
                 if (ones == tens && tens == hundreds) {
                     if (count[ones] >= 3) {
                         ans++;
                     }
-                } 
-                else if (ones == tens) {
+                } else if (ones == tens) {
                     if (count[ones] >= 2) {
                         ans++;
                     }
-                } 
-                else if (ones == hundreds) {
+                } else if (ones == hundreds) {
                     if (count[ones] >= 2) {
                         ans++;
                     }
-                } 
-                else if (tens == hundreds) {
+                } else if (tens == hundreds) {
                     if (count[tens] >= 2) {
                         ans++;
                     }
-                } 
-                else {
+                } else {
                     ans++;
                 }
             }
         }
-
         return ans;
+    }
+
+    public static void main(String[] a) {
+        LC3483_Unique3DigitNums solution = new LC3483_Unique3DigitNums();
+        int[] digits = { 1, 2, 3, 4, 5 };
+        int result = solution.totalNumbers(digits);
+        System.out.println("Total unique 3-digit numbers: " + result);
     }
 }
