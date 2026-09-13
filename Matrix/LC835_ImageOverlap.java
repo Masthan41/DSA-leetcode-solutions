d2e27f724914cd24efeac9adbb1678a5a41f54ac
Md Masthan <mdmastan633@gmail.com>
2026-09-13 23:51:51 +0530

    Added image overlap code

--- Matrix/LC835_ImageOverlap.java
+++ Matrix/LC835_ImageOverlap.java
@@ -1,3 +1,10 @@
+/*
+Leetcode 835 - Image Overlap
+Approach - We can fix one image and move the other image in all possible directions. For each shift, we can count the number of overlapping 1s.
+Time Complexity - O(n^4) where n is the dimension of the image (since we are shifting in both x and y directions and counting overlaps)
+Space Complexity - O(1) since we are using a constant amount of extra space
+*/
+
 class LC835_ImageOverlap {
     public int shiftAndCount(int[][] A, int[][] B, int x_shift, int y_shift) {
         int n = A.length;
@@ -25,18 +32,19 @@ class LC835_ImageOverlap {
         int maxOverlap = 0;
         for (int x_shift = 0; x_shift < n; x_shift++) {
             for (int y_shift = 0; y_shift < n; y_shift++) {
-                //Fix A, move B left and right
+                // Fix A, move B left and right
                 maxOverlap = Math.max(maxOverlap, shiftAndCount(img1, img2, x_shift, y_shift));
-                //Fix B, move A
+                // Fix B, move A
                 maxOverlap = Math.max(maxOverlap, shiftAndCount(img2, img1, x_shift, y_shift));
             }
         }
         return maxOverlap;
     }
+
     public static void main(String[] args) {
         LC835_ImageOverlap obj = new LC835_ImageOverlap();
-        int[][] img1 = {{1, 1, 0}, {0, 1, 0}, {0, 1, 0}};
-        int[][] img2 = {{0, 0, 0}, {0, 1, 1}, {0, 0, 1}};
+        int[][] img1 = { { 1, 1, 0 }, { 0, 1, 0 }, { 0, 1, 0 } };
+        int[][] img2 = { { 0, 0, 0 }, { 0, 1, 1 }, { 0, 0, 1 } };
         System.out.println(obj.largestOverlap(img1, img2));
     }
 }
