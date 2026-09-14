64c2440177133605b1947f59405d9734e48ba40c
Md Masthan <mdmastan633@gmail.com>
2026-09-14 22:05:24 +0530

    Added Rectangle overlap code

--- Matrix/LC836_RectangleOverlap.java
+++ Matrix/LC836_RectangleOverlap.java
@@ -1,3 +1,11 @@
+/*
+Leetcode 836 - Rectangle Overlap
+Approach - We can check if two rectangles overlap by comparing their coordinates. Two rectangles do not overlap if one is completely to the left, right, above, or below the other. Therefore, we can check the conditions for non-overlap and return the negation of that.
+Time Complexity - O(1) since we are performing a constant number of comparisons
+Space Complexity - O(1) since we are using a constant amount of extra space
+*/
+
+
 class LC836_RectangleOverlap {
     public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
         return rec1[0] < rec2[2] && rec2[0] < rec1[2] &&
